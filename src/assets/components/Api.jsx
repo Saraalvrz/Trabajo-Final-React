c05b87d30db41a091f1afab3eb782648088e9c45
cristian <cdah101206@gmail.com>
2024-12-03 20:15:31 +0100

    cambios en home.jsx, home.css y api.jsx

--- src/assets/components/Api.jsx
+++ src/assets/components/Api.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 const ApiComponent = async () => {
     try {
-        const response = await fetch('https://api.escuelajs.co/api/v1/products');
-        const data = await response.json();
-        return data; 
+        const productResponse = await fetch('https://api.escuelajs.co/api/v1/products');
+        const userResponse = await fetch('https://api.escuelajs.co/api/v1/users');
+        
+        const products = await productResponse.json()
+        const users = await userResponse.json();
+
+        return { products, users };
     } catch (error) {
         console.error('Error al obtener productos:', error);
-        return []; 
+        return {products: [], users: []}; 
     }
 };
+
 
 export default ApiComponent;
